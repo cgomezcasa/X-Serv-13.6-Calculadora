5f5be1caa05a935a5c9abb844a706793afce921f
cgomezcasa <cayegomez1995@gmail.com>
2018-02-01 16:19:03 +0100

    calculadora.py v2: excepciones contempladas

--- calculadora.py
+++ calculadora.py
@@ -2,12 +2,25 @@
 
 import sys
 from sys import argv
+
+if len(argv) != 4: 
+    sys.exit("Número de argumentos inválido.")
+
 operacion = argv[1]
-op1 = float(argv[2])
-op2 = float(argv[3])
-if operacion == "sumar": print(op1, "+", op2, "=", op1+op2)
-elif operacion == "restar": print(op1, "-", op2, "=", op1-op2)
-elif operacion == "multiplicar": print(op1, "*", op2, "=", op1*op2)
+
+try:
+    op1 = float(argv[2])
+    op2 = float(argv[3])
+except ValueError:
+    sys.exit("Tipo de operando inválido, solo es posible int o float.")
+
+    
+if operacion == "sumar": 
+    print(op1, "+", op2, "=", op1+op2)
+elif operacion == "restar": 
+    print(op1, "-", op2, "=", op1-op2)
+elif operacion == "multiplicar": 
+    print(op1, "*", op2, "=", op1*op2)
 elif operacion == "dividir":
     if op2 == 0: sys.exit('No se puede dividir entre 0.')
     else: print(op1, "/", op2, "=", op1/op2)
