cd3042c05985f22e72b2bbeeb09877e7be11a7cd
cgomezcasa <cayegomez1995@gmail.com>
2018-02-01 16:27:35 +0100

    calculadora.py v4: tras check.py

--- calculadora.py
+++ calculadora.py
@@ -13,7 +13,7 @@ try:
     op2 = float(argv[3])
 except ValueError:
     sys.exit("Tipo de operando inválido, solo es posible int o float.")
-  
+
 if operacion == "sumar":
     print(op1, "+", op2, "=", op1+op2)
 elif operacion == "restar":
@@ -22,7 +22,8 @@ elif operacion == "multiplicar":
     print(op1, "*", op2, "=", op1*op2)
 elif operacion == "dividir":
     if op2 == 0: sys.exit('No se puede dividir entre 0.')
-    else: print(op1, "/", op2, "=", op1/op2)
+    else: 
+        print(op1, "/", op2, "=", op1/op2)
 else:
     sys.exit('Esta operación no está contemplada.')
     
