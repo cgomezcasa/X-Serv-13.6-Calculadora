c5fb41edaf053393344f93c4e2b4c09a020bf01c
cgomezcasa <cayegomez1995@gmail.com>
2018-02-01 16:06:05 +0100

    calculadora.py v1

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+from sys import argv
+operacion = argv[1]
+op1 = float(argv[2])
+op2 = float(argv[3])
+if operacion == "sumar": print(op1, "+", op2, "=", op1+op2)
+elif operacion == "restar": print(op1, "-", op2, "=", op1-op2)
+elif operacion == "multiplicar": print(op1, "*", op2, "=", op1*op2)
+elif operacion == "dividir":
+    if op2 == 0: sys.exit('No se puede dividir entre 0.')
+    else: print(op1, "/", op2, "=", op1/op2)
+else:
+    sys.exit('Esta operación no está contemplada.')
+    
